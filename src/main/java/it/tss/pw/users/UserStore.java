785c651bba733134bd02c7da625907d586cd926b
marta <marta@DESKTOP-V559LKT.homenet.telecomitalia.it>
2020-05-05 13:00:17 +0200

    Creazione User

---
+++ src/main/java/it/tss/pw/users/UserStore.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.tss.pw.users;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+
+
+/**
+ *
+ * @author marta
+ */
+@ApplicationScoped
+public class UserStore {
+    private final Map <Long,User> users = new HashMap<>();
+   
+    @PostConstruct
+    public void start(){
+        Stream.of(new User (1l,"marta","ml"), new User (2l,"mario","mr"), new User (3l,"stefano","sl")).
+                forEach (v->users.put(v.getId(),v));
+        }
+    
+      public Collection <User> all(){
+            return users.values();
+    }
+      
+      public User create (User u) {
+        return users.putIfAbsent(u.getId(),u);
+      }
+      
+      public void delete (Long id){
+          users.remove(id);
+      }
+      
+}
